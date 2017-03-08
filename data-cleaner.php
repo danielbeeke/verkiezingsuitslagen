3aad183d2a02a98c84f5225780a2dee0dc371dcd
Daniel Beeke <daniel@studiofonkel.nl>
2017-03-08 21:32:48 +0100

    before switching to nodejs

--- data-cleaner.php
+++ data-cleaner.php
@@ -13,16 +13,27 @@ createCleanFiles($years);
 compareHeaders($years);
 
 function compareHeaders ($years) {
+  $all_columns = [];
+
   foreach ($years as $year) {
     $excel = new SimpleExcel('csv');
     $excel->parser->loadFile('cache/' . $year . '.csv');
     $headers = $excel->parser->getRow(1);
+
+    foreach ($headers as $header) {
+      $all_columns[$header] = $header;
+    }
   }
+
+  unset($all_columns['']);
+  $all_columns = array_values($all_columns);
+  sort($all_columns);
+
+  print_r($all_columns);
 }
 
 function createCleanFiles ($years) {
   foreach ($years as $year) {
-    echo $year . "\n";
     $csv_contents = file_get_contents('data/' . $year . '.csv');
     $csv_exploded = explode("\n\n\n", $csv_contents);
     $file_contents = str_replace("\n" . 'This file is created with Aspose.Cells for evaluation only with an Evaluation License. 
