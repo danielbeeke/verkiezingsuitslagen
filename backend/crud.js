e4980d165d8362046c01899953e6b5e31009211e
Daniel Beeke <daniel@studiofonkel.nl>
2017-03-12 17:21:45 +0100

    onwards

--- backend/crud.js
+++ backend/crud.js
@@ -5,12 +5,13 @@ var crud = {
         var year = req.params.year;
 
         db.query(
-            "SELECT c.name as city, json_agg(json_build_object('votes', votes, 'party_alias', pa.alias)) AS votes " +
+            "SELECT c.name as city, valid_votes, invalid_votes, entitled_voters, attendance, json_agg(json_build_object('votes', votes, 'party_alias', pa.alias)) AS votes " +
             'FROM city_votes cv ' +
             'JOIN party_aliases pa ON cv.party_alias_id = pa.id ' +
             'JOIN cities c ON cv.city_id = c.id ' +
-            'WHERE cv.year = ${year} ' +
-            'GROUP BY c.name ' +
+            'JOIN city_votes_info cvi ON cvi.city_id = c.id AND cv.year = cvi.year ' +
+            'WHERE cv.year = ${year}' +
+            'GROUP BY (c.name, valid_votes, invalid_votes, entitled_voters, attendance) ' +
             'ORDER BY c.name LIMIT 100',
         {
             year: year
